6d757d0c0f5bb89b204d1481e12e4cc0ddc77a88
PeteJames21 <peetjames21@gmail.com>
2024-01-31 19:07:33 +0300

    Add function for computing totals by subcategory

--- app/utils/stats.py
+++ app/utils/stats.py
@@ -1,3 +1,20 @@
+from collections import defaultdict
+
+
 def get_transactions_total(transactions: list[dict]):
     """Compute the sum of the value of all the given transactions."""
     return sum(int(t['amount']) for t in transactions)
+
+
+def get_totals_by_subcategories(transactions: list[dict]) -> list[tuple[str, int]]:
+    """
+    Compute totals by subcategory.
+
+    Sample output: [('food', 100), ('electricity', 40), ...]
+    """
+    totals = defaultdict(int)
+    for t in transactions:
+        totals[t['subcategory']] += int(t['amount'])
+
+    # Sort results by amount in descending order
+    return sorted(list(totals.items()), key=lambda x: x[1], reverse=True)
